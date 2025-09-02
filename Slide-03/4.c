4c967ad83712c08fb07362fe669f978bbc8f5fd5
GranularBow8193 <71673426+GranularBow8193@users.noreply.github.com>
2025-09-02 15:17:17 -0300

    Criar 4.c

---
+++ Slide-03/4.c
@@ -0,0 +1,21 @@
+/*
+
+1) Qual é a diferença entre tipagem estática e dinâmica?
+
+A tipagem estática verifica o tipo da variável na compilação, antes da execução. Já a tipagem dinâmica verifica na execução.
+
+
+
+2) Dê exemplos de linguagens que usem tipagem estática e exemplos de linguagens que usem tipagem dinâmica.
+
+Tipagem estática: C, C#, C++, Java
+
+Tipagem dinâmica: Python, JavaScript, PHP
+
+
+
+3) Por quê uma linguagem com tipagem estática é tipicamente mais rápida do que uma com tipagem dinâmica? Justifique usando um exemplo.
+
+A tipagem dinâmica precisa verificar os tipos de dados enquanto executa, o que torna um peso a mais na execução, enquanto na estática ele já foi verificado antes da execução, deixando o processo mais rápido
+
+*/
