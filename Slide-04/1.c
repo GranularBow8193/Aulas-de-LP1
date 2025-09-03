cc2b014561859436ae9470ef11180ed5686c016c
GranularBow8193 <71673426+GranularBow8193@users.noreply.github.com>
2025-09-03 10:42:05 -0300

    Create 1.c

---
+++ Slide-04/1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+int eh_primo(int num){
+
+      for (int i = 2; i <= sqrt(num); i++){
+
+            if (num % i == 0){
+                  return 0;
+            }
+            else if(num == 1){
+                  return 0;
+            }
+            
+      }
+      return 1;
+}
+
+int main(void){
+
+      int num;
+
+      printf("Digite um número para descobrir se é primo: ");
+      scanf("%d", &num);
+
+      if(eh_primo(num) == 0){
+            printf("O número não é primo");
+      }
+      else{
+            printf("O número é primo");
+      }
+
+      return 0;
+}
