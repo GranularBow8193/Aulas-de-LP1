0b12d651dc23e379d741607950c61001205c130a
GranularBow8193 <71673426+GranularBow8193@users.noreply.github.com>
2025-09-25 21:44:37 -0300

    Update 1.c

--- Slide-06/1.c
+++ Slide-06/1.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
 
-int main(void){
+void preenche (int* xs, int n){
 
+    for (int i = 0; i < n; i++){
 
-return 0;
+        printf("Elemento %d: ",i + 1);
+        scanf("%d", xs + i);
+    }
+}
+
+float media(int *xs, int n){
+
+    float soma = 0;
+    for (int i = 0; i < n; i++){
+        soma += *(xs + i);
+    }
+    return (soma / n);
+}
+
+int main(){
+
+    int vec[100];
+    int tamanho;
+    printf("Qual o tanho do vetor (de 1 até 100)? ");
+    scanf("%d",&tamanho);
+
+    preenche(vec, tamanho);
+    printf("\nA média dos itens do vetor é: %.2f \n\n", media(vec,tamanho));
+
+    //printar o vetor para visualização (não solicitado)
+    for (int i = 0; i < tamanho; i++){
+        printf("%d ",*(vec+i));
+    }
+    printf("\n");
+    return 0;
 }
