eb4c0f3e85073f50788a094af3cdfb51bfd5915c
GranularBow8193 <71673426+GranularBow8193@users.noreply.github.com>
2025-09-28 12:02:32 -0300

    Update Strings - 1.c

--- Slide-06/Strings - 1.c
+++ Slide-06/Strings - 1.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int tamanho(char* str){
+
+    int contador = 0;
+    for (int i = 0; i < 100; i++){
+
+        if (str[i] != '\0'){
+            contador++;
+        }
+    }
+    
+    return contador;
+}
+/*
+int main(){
+
+    char string[100];
+    printf("Digite uma frase: ");
+    scanf("%s",string);
+    printf("%d\n",tamanho(string));
+
+    return 0;
+}
+*/
