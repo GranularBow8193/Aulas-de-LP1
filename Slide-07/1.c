0cb95e28f364667c8eb7ad8b2813461bb3e2883a
GranularBow8193 <71673426+GranularBow8193@users.noreply.github.com>
2025-09-29 21:00:58 -0300

    Update 1.c

--- Slide-07/1.c
+++ Slide-07/1.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
-int main(void){
+struct Personagem {
+    int forca;
+    int energia;
+    int experiencia;
+};
 
+int main(){
 
-return 0;
+      struct Personagem p1 = {0};
+      //Use as duas formas de inicializar structs:
+      //acessando cada campo individualmente pelo nome
+      p1.forca = 10;
+      p1.energia = 50;
+      p1.experiencia = 30;
+
+      printf("Força: %d\n",p1.forca);
+      printf("Energia: %d\n",p1.energia);
+      printf("Experiência: %d\n",p1.experiencia);
+
+      //usando as chaves onde cada campo é separado por vírgula
+      struct Personagem p2 = {10, 50, 30};
+      
+      printf("\nForça: %d\n",p1.forca);
+      printf("Energia: %d\n",p1.energia);
+      printf("Experiência: %d\n",p1.experiencia);
+      
+      return 0;
 }
