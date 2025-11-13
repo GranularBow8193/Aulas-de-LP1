702f6fd046619847a42aafcaf78f21d7f5467704
GranularBow8193 <71673426+GranularBow8193@users.noreply.github.com>
2025-11-13 14:37:14 -0300

    Arrays
    Implement a function to fill a 3x3 integer array with user input and display its contents.

---
+++ Slide-06/Arrays - 1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#define L 3
+#define C 3
+
+//Crie uma função preenche que receba um array bidimensional arr de inteiros com uma dimensão fixa (LxC), e preencha o array com l*c números lidos do teclado
+//array 3x3
+void preenche (int arr[L][C], int l, int c){
+
+    for (int i = 0; i < l; i++){
+            for (int j = 0; j < c; j++){
+                printf("Digite o valore da posição linha %d, coluna %d ",i+1,j+1);
+                scanf("%d",&arr[i][j]);
+            }
+    }
+}
+
+int main(){
+
+    int vetor[L][C];
+    preenche(vetor, 3, 3);
+
+    for (int i = 0; i < L; i++){
+            for (int j = 0; j < C; j++){
+                printf("%d\n", vetor[i][j]);
+            }
+    }
+    return 0;
+}
