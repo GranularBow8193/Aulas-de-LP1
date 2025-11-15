8e2ae05cbf8a06574aa7fedc356639e42cd18dd4
GranularBow8193 <71673426+GranularBow8193@users.noreply.github.com>
2025-11-15 00:05:40 -0300

    Arrays

--- Slide-06/Arrays - 1.c
+++ Slide-06/Arrays - 1.c
@@ -1,29 +1,34 @@
 #include <stdio.h>
 
-#define L 3
-#define C 3
+#define L 100
+#define C 100
 
 //Crie uma função preenche que receba um array bidimensional arr de inteiros com uma dimensão fixa (LxC), e preencha o array com l*c números lidos do teclado
-//array 3x3
 void preenche (int arr[L][C], int l, int c){
 
-    for (int i = 0; i < l; i++){
+      for (int i = 0; i < l; i++){
             for (int j = 0; j < c; j++){
                 printf("Digite o valore da posição linha %d, coluna %d: ",i+1,j+1);
                 scanf("%d",&arr[i][j]);
             }
-    }
+      }
+      printf("\n");
 }
 
 int main(){
 
-    int vetor[L][C];
-    preenche(vetor, 3, 3);
+      int vetor[L][C];
+      int linhas; int colunas;
+      printf("Quantas linhas tem o vetor? ");
+      scanf("%d",&linhas);
+      printf("Quantas colunas tem o vetor? ");
+      scanf("%d",&colunas);
+      preenche(vetor, linhas, colunas);
 
-    for (int i = 0; i < L; i++){
-            for (int j = 0; j < C; j++){
+      for (int i = 0; i < linhas; i++){
+            for (int j = 0; j < colunas; j++){
                 printf("%d\n", vetor[i][j]);
             }
-    }
-    return 0;
+      }
+      return 0;
 }
