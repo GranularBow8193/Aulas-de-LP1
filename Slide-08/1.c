162dd44b4b3260f834a3fc955efa1e1d3fa5c132
Miguel <71673426+GranularBow8193@users.noreply.github.com>
2025-12-09 10:33:38 -0300

    Update 1.c

--- Slide-08/1.c
+++ Slide-08/1.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <string.h>
 
+int main(){
+
+    char str[26];
+    FILE* f;
+
+    printf("String com 25 caracteres no máximo: ");
+    scanf("%s",str);
+
+    f = fopen("arquivo.txt","wb");
+    int tamanho = strlen(str);
+
+    for (int i = 0; i < tamanho; i++){
+        fputc(str[i],f);
+    }
+    fclose(f);
+
+    printf("String escrita no arquivo com sucesso.");
+
+    return 0;
+}
